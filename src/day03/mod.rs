52e1e9f8448b7428da2d16dcaa0759ea74f9fdb4
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-12-03 13:13:53 +0100

    Solve day 3 part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -2,10 +2,19 @@ mod input;
 
 use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Rucksack {
     first: HashSet<char>,
     second: HashSet<char>
+}
+
+impl Rucksack {
+    pub fn content(&self) -> HashSet<char> {
+        self.first
+            .union(&self.second)
+            .map(|c| c.clone())
+            .collect()
+    }
 }
 
 fn rucksack(line: &str) -> Rucksack {
@@ -36,7 +45,64 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
+#[derive(Debug, Clone)]
+struct Group {
+    first: Rucksack,
+    second: Rucksack,
+    third: Rucksack
+}
+
+fn groups(rucksacks: Vec<Rucksack>) -> Vec<Group> {
+    let mut groups = vec![];
+    let mut i = 0;
+    let mut current: Vec<Rucksack> = vec![];
+    for rucksack in rucksacks {
+        current.push(rucksack);
+        if i / 2 == 1 {
+            i = 0;
+            groups.push(Group {
+                first: current[0].clone(),
+                second: current[1].clone(),
+                third: current[2].clone()
+            });
+            current = vec![];
+            continue;
+        }
+        i += 1;
+    }
+    groups
+}
+
+fn group_in_common(group: &Group) -> char {
+    let first = group.first.content();
+    let second = group.second.content();
+    let third = group.third.content();
+
+    first
+        .intersection(&second)
+        .map(|c| c.clone())
+        .collect::<HashSet<char>>()
+        .intersection(&third)
+        .last()
+        .unwrap()
+        .clone()
+}
+
+fn part2(input: &str) -> usize {
+    let rucksacks = input
+        .split('\n')
+        .map(rucksack);
+
+    groups(rucksacks.collect())
+        .iter()
+        .map(group_in_common)
+        .map(priority)
+        .sum()
+}
+
 pub fn test() {
     assert_eq!(part1(input::TEST), 157);
-    assert_eq!(part1(input::REAL), 0);
+    assert_eq!(part1(input::REAL), 7746);
+    assert_eq!(part2(input::TEST), 70);
+    assert_eq!(part2(input::REAL), 2604);
 }
