f0796bde68aaa43983d32de8344bcc9555c6bf9d
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-12-04 14:53:06 +0100

    Solve day 4 part 1

---
+++ src/day04/mod.rs
@@ -0,0 +1,44 @@
+mod input;
+
+use std::collections::HashSet;
+
+#[derive(Debug, Clone)]
+struct Pair {
+    first: HashSet<u32>,
+    second: HashSet<u32>
+}
+
+fn assigned(spec: &str) -> HashSet<u32> {
+    let (start, end) = spec.split_once('-').unwrap();
+
+    let start = start.parse::<u32>().unwrap();
+    let end = end.parse::<u32>().unwrap();
+    (start..(end + 1)).collect::<HashSet<u32>>()
+}
+
+fn pair(line: &str) -> Pair {
+    let (first, second) = line.split_once(',').unwrap();
+
+    Pair {
+        first: assigned(first),
+        second: assigned(second)
+    }
+}
+
+fn fully_overlaps(pair: &Pair) -> bool {
+    pair.first.is_subset(&pair.second) ||
+    pair.second.is_subset(&pair.first)
+}
+
+fn part1(input: &str) -> usize {
+    input
+        .split('\n')
+        .map(pair)
+        .filter(fully_overlaps)
+        .count()
+}
+
+pub fn test() {
+    assert_eq!(part1(input::TEST), 2);
+    assert_eq!(part1(input::REAL), 494);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     day01::test();
     day02::test();
     day03::test();
+    day04::test();
 }
