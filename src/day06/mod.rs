58cd430cbfbe5665300f2dafe1f9726fd1c72ff8
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-12-06 20:14:14 +0100

    Solve day 6 part 2

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -2,15 +2,15 @@ mod input;
 
 use std::collections::HashSet;
 
-fn from(i: usize) -> usize {
-    match i < 3 {
+fn from(i: usize, packet_length: usize) -> usize {
+    match i < packet_length - 1 {
         true => 0,
-        false => i - 3
+        false => i - (packet_length - 1)
     }
 }
 
-fn is_marker(s: &str) -> bool {
-    if s.len() < 4 {
+fn is_marker(s: &str, min_length: usize) -> bool {
+    if s.len() < min_length {
         return false;
     }
 
@@ -27,14 +27,14 @@ fn is_marker(s: &str) -> bool {
     true
 }
 
-fn part1(input: &str) -> usize {
+fn marker_pos(input: &str, packet_length: usize) -> usize {
     for i in 0..input.len() {
-        let from = from(i);
+        let from = from(i, packet_length);
         let to = i+1;
 
         let s = &input[from..to];
 
-        if is_marker(s) {
+        if is_marker(s, packet_length) {
             return i + 1;
         }
     }
@@ -42,7 +42,17 @@ fn part1(input: &str) -> usize {
     0
 }
 
+fn part1(input: &str) -> usize {
+    marker_pos(input, 4)
+}
+
+fn part2(input: &str) -> usize {
+    marker_pos(input, 14)
+}
+
 pub fn test() {
     assert_eq!(part1(input::TEST), 7);
     assert_eq!(part1(input::REAL), 1647);
+    assert_eq!(part2(input::TEST), 19);
+    assert_eq!(part2(input::REAL), 0);
 }
